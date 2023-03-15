db15ec24baa703b8bfb081c872e2d8e9f673a5cc
thick <thickishwharf9@gmail.com>
2023-03-15 19:05:13 +0900

    debug api call

--- FRONT/host/src/apis/auth.ts
+++ FRONT/host/src/apis/auth.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const loginWithCredential = async (credential: string) => {
   const response = await axios.post(`${process.env.REACT_APP_SERVER}/api/user`, {
-    data: credential,
+    credential: credential,
   });
   return response;
 };
