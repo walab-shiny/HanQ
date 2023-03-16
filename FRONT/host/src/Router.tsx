7a1508f168c7fd92666dac963cc23eb887e231b5
Songdabin <thdekqls1222@naver.com>
2023-03-16 14:05:50 +0900

    feat[#12] : event list page

--- FRONT/host/src/Router.tsx
+++ FRONT/host/src/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import Event from './pages/Event';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/event" element={<Event />} />
       </Routes>
     </BrowserRouter>
   );
--- FRONT/host/src/components/Dashboard/Header.tsx
+++ FRONT/host/src/components/Dashboard/Header.tsx
@@ -3,11 +3,15 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { Box, Typography } from '@mui/material';
 import Search from './Search';
 
-export default function Header() {
+interface propsType {
+  page: string;
+}
+
+export default function Header(props: propsType) {
   return (
     <Box display="flex" alignItems="center" m={2} justifyContent="space-between">
       <Box display="flex" alignItems="center" ml={6} gap={1}>
-        <Typography sx={{ fontSize: '1.2rem', fontWeight: 'bold' }}>대시보드</Typography>
+        <Typography sx={{ fontSize: '1.2rem', fontWeight: 'bold' }}>{props.page}</Typography>
         <Search />
       </Box>
       <Box display="flex" gap={3} mr={5}>
