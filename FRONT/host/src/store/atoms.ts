5f82c193e4c87e709192a2f0a6c43ab408d1ce68
Seokmin Jung <79754613+shine-jung@users.noreply.github.com>
2023-03-20 22:09:34 +0900

    [#18] chore: host & user 프로젝트 폴더 병합

--- FRONT/host/src/store/atoms.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil';
-
-export const themeModeState = atom<'light' | 'dark'>({
-  key: 'themeMode',
-  default: (localStorage.getItem('themeMode') ?? 'light') as 'light' | 'dark',
-});
--- FRONT/user/src/components/LogoutBtn.tsx
+++ FRONT/user/src/components/LogoutBtn.tsx
@@ -2,12 +2,16 @@ import { Button } from '@mui/material';
 import { googleLogout } from '@react-oauth/google';
 import { useResetRecoilState } from 'recoil';
 import { authState } from '../store/auth';
+import { userState } from '../store/user';
 
 export default function LogoutBtn() {
   const resetCredential = useResetRecoilState(authState);
+  const resetUser = useResetRecoilState(userState);
   const removeCredential = () => {
     localStorage.removeItem('credential');
     resetCredential();
+    resetUser();
+    window.location.reload();
   };
   const handleLogout = () => {
     googleLogout();
