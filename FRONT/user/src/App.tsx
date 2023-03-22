f7708cb077d3113a444e51a41c2ee8e3c8735fa2
Songdabin <thdekqls1222@naver.com>
2023-03-22 19:36:35 +0900

    chore[#12] : host to user

--- FRONT/user/src/App.tsx
+++ FRONT/user/src/App.tsx
@@ -1,14 +1,19 @@
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { SnackbarProvider } from 'notistack';
 import ThemeProvider from './theme';
 import Router from './Router';
 
 function App() {
+  const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID ?? '';
+
   return (
-    <ThemeProvider>
-      <SnackbarProvider maxSnack={3}>
-        <Router />
-      </SnackbarProvider>
-    </ThemeProvider>
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <ThemeProvider>
+        <SnackbarProvider maxSnack={3}>
+          <Router />
+        </SnackbarProvider>
+      </ThemeProvider>
+    </GoogleOAuthProvider>
   );
 }
 
--- FRONT/user/src/Router.tsx
+++ FRONT/user/src/Router.tsx
@@ -1,13 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ThemeModeToggle from './components/Header/ThemeModeToggle';
-import Main from './pages/Main';
+import { useRecoilValue } from 'recoil';
+import Dashboard from './pages/Dashboard';
+import Event from './pages/Event';
+import EventDetail from './pages/EventDetail';
+import Report from './pages/Report';
+import Login from './pages/Login';
+import { authState } from './store/auth';
 
 function Router() {
+  const userId = useRecoilValue(authState);
   return (
     <BrowserRouter>
-      <ThemeModeToggle />
       <Routes>
-        <Route path="/" element={<Main />} />
+        {userId ? (
+          <>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/event" element={<Event />} />
+            <Route path="/event/detail" element={<EventDetail />} />
+            <Route path="/report" element={<Report />} />
+          </>
+        ) : (
+          <Route path="*" element={<Login />} />
+        )}
       </Routes>
     </BrowserRouter>
   );
