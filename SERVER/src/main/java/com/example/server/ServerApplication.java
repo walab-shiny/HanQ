120d6aa4a8fc260ac650ac30ef336c3f02af0132
juyoung88 <76582732+juyoung88@users.noreply.github.com>
2023-12-01 16:15:51 +0900

    refactor: deleted unused import statement (#125)

--- SERVER/src/main/java/com/example/server/ServerApplication.java
+++ SERVER/src/main/java/com/example/server/ServerApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
